1b8201aee5095160d791c9aa1b5aceb1185d32bb
kevin2li1024 <kevin2li1024@gmail.com>
2022-06-20 19:25:03 +0800

    update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
 import os
 import sys
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser("hello")
+    parser.add_argument("-s", type=str, default='hello world!')
+    return parser.parse_args()
 
 if __name__ == '__main__':
-    print("Hello, world!")
+    args = parse_args()
+    print(args.s)
